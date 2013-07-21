9fb9ea3d3708f8084051c970732508f968dbe071
teknik tjek <tekniktjek@gmail.com>
2013-07-21 13:41:12 +0200

    rock7

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! menza rocks');
+  response.send('Hello World! menza rocks - githubconnect');
 });
 
 var port = process.env.PORT || 5000;
